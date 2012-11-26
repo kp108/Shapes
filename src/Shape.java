3c0065b7412ad4ae19b5245f619234f38ecdec1f
Khoi Pham <khoi.pham108@gmail.com>
2012-11-26 14:34:56 -0700

    Initial commit

---
+++ src/Shape.java
@@ -0,0 +1,17 @@
+public class Shape 
+{
+	protected double area;
+	protected double perimeter;
+
+	public Shape() 
+	{
+		area = perimeter = 0;
+	}
+
+	public String toString() 
+	{
+
+		return "area = " + area + " \t perimeter = " + perimeter;
+
+	}
+}
